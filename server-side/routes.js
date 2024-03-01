1b690feb569ee95fe48bcc8ec8b63add2f8499d4
ananyatewari <ananyatewari0205@gmail.com>
2024-03-01 12:00:30 +0530

    made a reach app

---
+++ server-side/routes.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const router = express.Router()
+const { connectToDataBase } = require('./db.js')
+const Moment = require('./schema.js')
+
+connectToDataBase()
+
+router.get('/', async (req, res) => {
+    try {
+        const times = await Moment.find()
+        res.json(times)
+    }
+    catch (error) {
+        res.json({error: 'An error has been caught- get'})
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const times = await Moment.findById(req.params.id)
+        res.json(times)
+    }
+    catch (error) {
+        res.json({error: "An error has been caught - getID"})
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const time = new Moment(req.body)
+        const saveMoment = await time.save()
+        res.status(201).json(saveMoment)
+    }
+    catch (error) {
+        res.send({error:"An error has been caught - post"})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const times = await Moment.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        if (!times) {
+            return res.status(400).send("No result found")
+        }
+        res.json(times)
+    }
+    catch (error) {
+        res.status(400).json({error: "An error has been caught"})
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const times = await Moment.findByIdAndUpdate(req.params.id, req.body, { new : true })    
+        if (!times) {
+            return res.status(400).json("No result found")
+        }
+        res.json(times)
+    }
+    catch (error) {
+        res.status(400).json({error: "An error has been caught"})
+    }
+})
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const times = await Moment.findByIdAndDelete(req.params.id)
+        if (!times) {
+            return res.status(400).send("No result found")
+        }
+        res.send("Item deleted successfully")
+    }
+    catch (error) {
+        res.status(400).json({error: "An error has been caught"})
+    }
+})
+
+
+module.exports = router
---
+++ server-side/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 3000
+const routes = require('./routes')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use('/api', routes)
+
+
+app.get('/ping', (req, res) => {
+  res.send(`<h1>Let's recall some embarrassing moments !</h1>`)
+})
+
+app.get('/', (req, res) => {
+  const status = mongoose.connection.readyState == 1 ? 'Connected 😎😎' : 'Disconnected 😓😓'
+  res.send(`Your DataBase connection status is: ${status}`)
+})
+
+app.listen(port, async () => {
+  // await connectToDataBase()
+  console.log(`The server running on PORT: ${port}`)
+})
+
+// module.exports =  { connectToDataBase } 
